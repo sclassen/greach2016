181051094657eb38efee9390829f142bebaee97f
Stephan Classen <st.classen@gmx.ch>
2016-03-11 00:52:43 +0100

    capture screenshot and add include it into html

--- hacking/src/main/groovy/demo/CaptureBlockMacroProcessor.groovy
+++ hacking/src/main/groovy/demo/CaptureBlockMacroProcessor.groovy
@@ -1,7 +1,10 @@
 package demo
 
+import geb.Browser
+import geb.report.ScreenshotReporter
 import org.asciidoctor.ast.AbstractBlock
 import org.asciidoctor.extension.BlockMacroProcessor
+import org.openqa.selenium.Dimension
 
 class CaptureBlockMacroProcessor extends BlockMacroProcessor {
     CaptureBlockMacroProcessor(String macroName, Map<String, Object> config) {
@@ -10,8 +13,22 @@ class CaptureBlockMacroProcessor extends BlockMacroProcessor {
 
     @Override
     protected Object process(AbstractBlock parent, String target, Map<String, Object> attributes) {
+        Browser.drive {
+            Browser browser = delegate as Browser
+            browser.driver.manage().window().size = new Dimension(800, 600)
+            browser.config.reporter = new ScreenshotReporter()
+            browser.config.reportsDir = '/tmp' as File
+
+            if (target) {
+                go target
+            }
+
+            report 'screenshot'
+        }
+
+
         createBlock(parent, "image", "", [
-                target: CaptureBlockMacroProcessor.classLoader.getResource('logo.png').toString()
+                target: 'file:/tmp/screenshot.png'
         ], [:])
     }
 }
